d485182f1525c351b1c3bdc936bb621ca7009747
Vitalii Litvinov <vitalii.litvinov@thetoadman.com>
2024-01-10 16:42:16 +0100

    added commandline and threadpool classes

---
+++ commandline.h
@@ -0,0 +1,277 @@
+﻿//=================================================================================================
+//===
+//=== args.h
+//===
+//=== Copyright (c) 2020-2023 by RangeSoft.
+//=== All rights reserved.
+//===
+//=== Litvinov "VeduN" Vitaly
+//===
+//=================================================================================================
+#ifndef _SIMPLEUTILS_COMMANDLINE_H_
+#define _SIMPLEUTILS_COMMANDLINE_H_
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace SmallUtils
+{
+
+class CommandLine
+{
+private:
+    struct Item
+    {
+        bool          isSwitch;
+        unsigned int  isSet;
+        std::string   fullname;
+        unsigned char shortname;
+        std::string   value;
+    };
+
+#ifdef SIMPLEUTILS_ARGS_AS_SINGLETON
+public:
+    static CommandLine& instance() { static CommandLine Singleton; return Singleton; }
+private:
+    CommandLine() = default;
+    CommandLine(const CommandLine&);
+    CommandLine& operator=(CommandLine&);
+#else
+public:
+    CommandLine() = default;
+#endif
+
+public:
+    virtual ~CommandLine() = default;
+
+public:
+    CommandLine& addSwitch(const std::string& name, unsigned char shortname)
+    {
+        auto item = findItem(name);
+
+        if (!item)
+        {
+            m_list.push_back(CommandLine::Item());
+            item = &m_list.back();
+        }
+
+        item->isSwitch  = true;
+        item->isSet     = 0;
+        item->fullname  = name;
+        item->shortname = shortname;
+        item->value     = "";
+
+        return *this;
+    }
+
+    CommandLine& addOption(const std::string& name, const unsigned char shortname, const std::string& default_value)
+    {
+        auto item = findItem(name);
+
+        if (!item)
+        {
+            m_list.push_back(CommandLine::Item());
+            item = &m_list.back();
+        }
+
+        item->isSwitch  = false;
+        item->isSet     = 0;
+        item->fullname  = name;
+        item->shortname = shortname;
+        item->value     = default_value;
+
+        return *this;
+    }
+
+    CommandLine& setSwitch(const std::string& name, bool isset)
+    {
+        auto item = findItem(name);
+
+        if (!item)
+        {
+            m_list.push_back(CommandLine::Item());
+            item = &m_list.back();
+            item->fullname = name;
+        }
+
+        item->isSet = isset;
+
+        return *this;
+    }
+
+    CommandLine& setOption(const std::string& name, const std::string& value)
+    {
+        auto item = findItem(name);
+
+        if (!item)
+        {
+            m_list.push_back(CommandLine::Item());
+            item = &m_list.back();
+
+            item->fullname = name;
+        }
+
+        item->value = value;
+        item->isSet = true;
+
+        return *this;
+    }
+
+    unsigned int isSet(const std::string& name) const
+    {
+        auto item = findConstItem(name);
+        return item ? item->isSet : false;
+    }
+
+    std::string  getOption(const std::string& name) const
+    {
+        auto item = findConstItem(name);
+        return item ? item->value : "";
+    }
+
+    std::string  getArgument(unsigned int num) const
+    {
+        return num < m_argument.size() ? m_argument[num] : "";
+    }
+
+    unsigned int getCountArgument() const
+    {
+        return m_argument.size();
+    }
+
+    unsigned int parse(unsigned int argc, const char **argv)
+    {
+	    Item* curarg = nullptr;
+
+	    for (unsigned int ii = 1; ii < argc; ++ii)
+        {
+            std::string arg = argv[ii];
+
+            if (arg[0] == '-')
+            {
+                if (arg.size() < 2 || arg[0] != '-')
+                {
+                    return ii;
+                }
+
+                if (arg[1] != '-')
+                {
+                    parseShort(arg);
+                    continue;
+                }
+
+                Item* oldarg = curarg;
+
+                curarg = parseFull(arg);
+
+                if (curarg)
+                {
+                    if (curarg->isSwitch)
+                    {
+                        curarg = nullptr;
+                    }
+                    else if (curarg->isSet)
+                    {
+                        curarg = nullptr;
+                    }
+                    continue;
+                }
+
+                if (oldarg)
+                {
+                    curarg = oldarg;
+                }
+            }
+
+            if (curarg)
+            {
+                curarg->value = arg;
+                ++curarg->isSet;
+                curarg = nullptr;
+                continue;
+            }
+            else
+            {
+                m_argument.push_back(arg);
+            }
+        }
+
+        return 0;
+    }
+
+private:
+    const Item* findConstItem(const std::string& name) const
+    {
+        for (auto& item : m_list)
+        {
+            if (item.fullname == name)
+            {
+                return &item;
+            }
+        }
+        return nullptr;
+    }
+
+    Item* findItem(const std::string& name)
+    {
+        for (auto& item : m_list)
+        {
+            if (item.fullname == name)
+            {
+                return &item;
+            }
+        }
+        return nullptr;
+    }
+
+    void parseShort(const std::string& arg)
+    {
+        std::string str = arg.substr(1);
+        for (auto& item : m_list)
+        {
+            int pos = str.find(item.shortname);
+            if(pos >= 0)
+            {
+                ++item.isSet;
+            }
+        }
+    }
+
+    Item* parseFull(const std::string& arg)
+    {
+        std::string name = arg.substr(2);
+        std::string value = "";
+        int pos = name.find('=');
+
+        if (pos >= 0)
+        {
+            value = name.substr(pos + 1);
+            name  = name.substr(0, pos);
+        }
+
+        for (auto& item : m_list)
+        {
+            if (item.fullname == name)
+            {
+                item.isSet += pos >= 0;
+                if (value.size())
+                {
+                    item.value = value;
+                }
+
+                return &item;
+            }
+        }
+
+        return nullptr;
+    }
+
+private:
+    std::vector<CommandLine::Item> m_list;
+    std::vector<std::string> m_argument;
+}
+
+}
+
+#endif
---
+++ threadpool.h
@@ -0,0 +1,165 @@
+//=================================================================================================
+//===
+//=== threadpool.h
+//===
+//=== Copyright (c) 2020-2023 by RangeSoft.
+//=== All rights reserved.
+//===
+//=== Litvinov "VeduN" Vitaly
+//===
+//=================================================================================================
+#ifndef _SIMPLEUTILS_THREADPOOL_H_
+#define _SIMPLEUTILS_THREADPOOL_H_
+#pragma once
+
+#include <iostream>
+#include <queue>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <future>
+#include <unordered_set>
+#include <atomic>
+#include <future>
+
+using TaskID = uint64_t;
+using LockGuard = std::lock_guard<std::mutex>;
+using LockUnique = std::unique_lock<std::mutex>;
+
+namespace SmallUtils
+{
+
+class ThreadPool
+{
+public:
+    ThreadPool(uint32_t numThreads)
+    {
+        if (!numThreads)
+        {
+            numThreads = getMaxThreads();
+            if (numThreads >= 2)
+            {
+                numThreads -= 2;
+            }
+        }
+        m_threads.reserve(numThreads);
+
+        for (uint32_t ii = 0; ii < numThreads; ++ii)
+        {
+            m_threads.emplace_back(&ThreadPool::run, this);
+        }
+    }
+
+    virtual ~ThreadPool()
+    {
+        m_exit = true;
+
+        for (auto& itm : m_threads)
+        {
+            m_queueCV.notify_all();
+            itm.join();
+        }
+    }
+
+    static uint32_t getMaxThreads()
+    {
+        return std::thread::hardware_concurrency();
+    }
+
+    uint32_t getThreadsCount() const
+    {
+        return m_threads.size();
+    }
+
+    template <typename Func, typename ...Args>
+    TaskID add_task(const Func& task_func, Args&&... args)
+    {
+        LockGuard lock(m_queueMutex);
+
+        TaskID task_idx = m_lastIndex++;
+
+        m_queue.emplace(std::async(std::launch::deferred, task_func, args...), task_idx);
+        m_queueCV.notify_one();
+
+        return task_idx;
+    }
+
+    void wait(TaskID task_id)
+    {
+        LockUnique lock(m_completedTaskMutex);
+
+        m_completedTaskCV.wait(lock, [this, task_id]()->bool
+        {
+            return m_completedTask.find(task_id) != m_completedTask.end();
+        });
+    }
+
+    void wait_all()
+    {
+        LockUnique lock(m_queueMutex);
+
+        m_completedTaskCV.wait(lock, [this]()->bool
+        {
+            std::lock_guard<std::mutex> task_lock(m_completedTaskMutex);
+            return m_queue.empty() && m_lastIndex == m_completedTask.size();
+        });
+    }
+
+    bool isTaskFinished(TaskID task_id)
+    {
+        LockGuard lock(m_completedTaskMutex);
+
+        if (m_completedTask.find(task_id) != m_completedTask.end())
+        {
+            return true;
+        }
+        return false;
+    }
+
+private:
+    void run()
+    {
+        while (!m_exit)
+        {
+            LockUnique lock(m_queueMutex);
+
+            m_queueCV.wait(lock, [this]()->bool { return !m_queue.empty() || m_exit; });
+
+            if (m_exit)
+            {
+                break;
+            }
+
+            if (!m_queue.empty())
+            {
+                auto elem = std::move(m_queue.front());
+                m_queue.pop();
+                lock.unlock();
+
+                elem.first.get();
+
+                LockGuard lock_result(m_completedTaskMutex);
+                m_completedTask.insert(elem.second);
+
+                m_completedTaskCV.notify_all();
+            }
+        }
+    }
+
+    std::queue<std::pair<std::future<void>, TaskID>> m_queue;
+    std::mutex m_queueMutex;
+    std::condition_variable m_queueCV;
+
+    std::unordered_set<TaskID> m_completedTask;
+    std::mutex m_completedTaskMutex;
+    std::condition_variable m_completedTaskCV;
+
+    std::vector<std::thread> m_threads;
+
+    std::atomic<bool> m_exit = false;
+    std::atomic<TaskID> m_lastIndex = 0;
+}
+
+}
+
+#endif

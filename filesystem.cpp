bb4887fed8a40674d5b978930ef22f77200c6cb8
Vitalii Litvinov <vitalii.litvinov@thetoadman.com>
2024-03-29 17:51:10 +0100

    added filesystem namespace

--- filesystem.cpp
+++ filesystem.cpp
@@ -8,6 +8,100 @@ namespace FileSystem
 const std::string DataBuffer::m_marker = "db";
 const std::string StringBuffer::m_marker = "sb";
 
+// Limits of packed bytes
+// 1 byte  0x7f
+// 2 bytes 0x3f ff
+// 3 bytes 0x1f ff ff
+// 4 bytes 0x0f ff ff ff
+// 5 bytes 0x07 ff ff ff ff
+// 6 bytes 0x03 ff ff ff ff ff
+// 7 bytes 0x01 ff ff ff ff ff ff
+// 8 bytes 0xff ff ff ff ff ff ff
+// 9 bytes 0x7f ff ff ff ff ff ff ff
+
+
+size_t packUnsigned(uint64_t val, uint8_t* buff)
+{
+    uint64_t maxVal = 0x7f;
+    int count = 1;
+    bool pushHiBit = false;
+
+    if (val > 0x7fffffffffffffff)
+    {
+        pushHiBit = true;
+        val = val & 0x7fffffffffffffff;
+    }
+
+    while (count <= 10)
+    {
+        if (val <= maxVal)
+        {
+            for (int ii = 0; ii < count - 1; ++ii)
+            {
+                *buff++ = (val & 0x7f) | 0x80;
+                val >>= 7;
+            }
+
+            *buff++ = (val & 0x7f) | (pushHiBit ? 0x80 : 0x00);
+
+            if (pushHiBit)
+            {
+                *buff++ = 0x01;
+            }
+            return count + pushHiBit;
+        }
+        else
+        {
+            ++count;
+            maxVal = (maxVal << 7) | 0x7f;
+        }
+    }
+
+    SU_ASSERT(true);
+    return 0;
+}
+
+size_t unpackUnsigned(uint64_t& v, uint8_t* buff)
+{
+    v = 0;
+    uint8_t ii = 0;
+    for (ii = 0; ii < 10; ++ii)
+    {
+        uint64_t t = 0;
+
+        t = *buff++;
+
+        bool exit = !(t & 0x80);
+
+        t &= 0x7F;
+        t <<= (7 * ii);
+        v |= t;
+
+        if (exit)
+        {
+            return ii + 1;
+        }
+    }
+
+    SU_ASSERT(true);
+    return 0;
+}
+
+std::string readCString(std::ifstream& ifs)
+{
+    char ch = -1;
+    std::string out = "";
+    while (ch)
+    {
+        ifs.read(&ch, 1);
+        if (ch)
+        {
+            out += ch;
+        }
+    }
+    return out;
+}
+
 DataBuffer& DataBuffer::add(const void* buf, size_t size)
 {
     const int8_t* v = reinterpret_cast<const int8_t*>(buf);
@@ -27,13 +121,34 @@ DataBuffer& DataBuffer::add(const void* buf, size_t size)
     return *this;
 }
 
+DataBuffer& DataBuffer::addCString(const char* c_str)
+{
+    while (c_str)
+    {
+        *this << *c_str;
+        ++c_str;
+    }
+    *this << (char)0;
+
+    return* this;
+}
+
+DataBuffer& DataBuffer::addPackUnsigned(uint64_t val)
+{
+    uint8_t buff[12];
+
+    add(buff, packUnsigned(val, buff));
+
+    return *this;
+}
+
 DataBuffer& DataBuffer::get(void* buf, size_t size)
 {
-    int8_t* v = reinterpret_cast<int8_t*>(buf);
+    uint8_t* v = reinterpret_cast<uint8_t*>(buf);
 
-    if (m_pos + size >= m_data.size())
+    if (m_pos + size > m_data.size())
     {
-        m_pos = m_data.size();
+        m_pos = m_data.size() + 1;
         return *this;
     }
 
@@ -45,8 +160,42 @@ DataBuffer& DataBuffer::get(void* buf, size_t size)
     return *this;
 }
 
-void DataBuffer::seek(int64_t pos, Seek way)
+DataBuffer& DataBuffer::getCString(std::string& str)
 {
+    char ch = -1;
+    
+    str = "";
+    while (ch)
+    {
+        *this >> ch;
+        if (ch)
+        {
+            str += ch;
+        }
+    }
+
+    return *this;
+}
+
+DataBuffer& DataBuffer::getPackUnsigned(uint64_t& v)
+{
+    if (isEof())
+    {
+        return *this;
+    }
+    auto count = unpackUnsigned(v, &m_data[m_pos]);
+
+    m_pos += count;
+    if (m_pos > m_data.size())
+    {
+        m_pos = m_data.size() + 1;
+        v = 0;
+    }
+    return *this;
+}
+
+void DataBuffer::seek(int64_t pos, Seek way)
+{   
     int64_t newpos = pos;
     if (way == Seek::Cur)
     {
@@ -64,6 +213,13 @@ bool DataBuffer::read(std::ifstream& ifs, uint64_t size)
     m_data.clear();
     m_data.resize(size);
     return ifs.read(reinterpret_cast<char*>(m_data.data()), size) ? true : false;
+}
+
+bool DataBuffer::write(std::ofstream& ofs) const
+{
+    ofs.write(reinterpret_cast<const char*>(m_data.data()), m_data.size());
+
+    return true;
 }
 
 // StringBuffer
@@ -108,19 +264,6 @@ std::vector<uint32_t> StringBuffer::add(const StringArray& v)
     return out;
 }
 
-ByteArray StringBuffer::getData() const
-{
-    DataBuffer db("");
-
-    db << m_data.size();
-    for (const auto& str: m_data)
-    {
-        db << str;
-    }
-
-    return db.getData();
-}
-
 bool StringBuffer::read(std::ifstream& ifs, uint64_t size)
 {
     DataBuffer db("");
@@ -144,81 +287,104 @@ bool StringBuffer::read(std::ifstream& ifs, uint64_t size)
     return true;
 }
 
+bool StringBuffer::write(std::ofstream& ofs) const
+{
+    DataBuffer db("");
+
+    db << m_data.size();
+    for (size_t ii = 0; ii < m_data.size(); ++ii)
+    {
+        db << m_data[ii];
+    }
+
+    return db.write(ofs);
+}
+
+// BaseHeader
+
+BaseHeader::~BaseHeader()
+{
+    for (auto item : m_buffer)
+    {
+        if (item->getParent() == this)
+        {
+            delete item;
+        }
+    }
+}
+
 bool BaseHeader::write(std::ofstream& ofs) const
 {
     BufferOffset header;
     std::vector<BufferOffset> offset;
-    DataBuffer out("");
+
+    ofs.write(m_type.c_str(), m_type.size() + 1);
+
+    uint8_t verMajor = getVersionMajor();
+    uint8_t verMinor = getVersionMinor();
+    ofs.write(reinterpret_cast<const char*>(&verMajor), sizeof(verMajor));
+    ofs.write(reinterpret_cast<const char*>(&verMinor), sizeof(verMajor));
+
+    header.pos = ofs.tellp();
+    header.size = 0;
+    ofs.write(reinterpret_cast<const char*>(&header.size), sizeof(header.size));
+    header.offset = ofs.tellp();
+
+    uint16_t count = static_cast<uint16_t>(m_buffer.size());
+    ofs.write(reinterpret_cast<const char*>(&count), sizeof(count));
 
     offset.resize(m_buffer.size());
-
-    out.add(m_type.c_str(), m_type.size() + 1);
-    out << getVersionMajor() << getVersionMinor();
-
-    header.pos = out.tell();
-    header.size = 0;
-    out << header.size;
-    header.offset = out.tell();
-    out << static_cast<uint8_t>(m_buffer.size());
-
     for (int ii = 0; ii < m_buffer.size(); ++ii)
     {
-        out << m_buffer[ii]->getMarker();
-        out << m_buffer[ii]->getName();
+        auto marker = m_buffer[ii]->getMarker();
+        auto name = m_buffer[ii]->getName();
 
-        offset[ii].pos = out.tell();
+        ofs.write(marker.c_str(), marker.size() + 1);
+        ofs.write(name.c_str(), name.size() + 1);
+
+        offset[ii].pos = ofs.tellp();
         offset[ii].offset = 0;
         offset[ii].size = 0;
-        out << offset[ii].offset << offset[ii].size;
+
+        ofs.write(reinterpret_cast<const char*>(&offset[ii].offset), sizeof(offset[ii].offset));
+        ofs.write(reinterpret_cast<const char*>(&offset[ii].size), sizeof(offset[ii].size));
     }
 
-    header.size = static_cast<uint32_t>(out.tell() - header.offset);
+    header.size = static_cast<uint32_t>(static_cast<uint64_t>(ofs.tellp()) - header.offset);
 
     for (int ii = 0; ii < m_buffer.size(); ++ii)
     {
-        ByteArray v = m_buffer[ii]->getData();
-        
-        offset[ii].offset = out.tell();
-        out.add(v.data(), v.size());
-        offset[ii].size = static_cast<uint32_t>(out.tell() - offset[ii].offset);
+        offset[ii].offset = ofs.tellp();
+        m_buffer[ii]->write(ofs);
+        offset[ii].size = static_cast<uint32_t>(static_cast<uint64_t>(ofs.tellp()) - offset[ii].offset);
     }
 
-    out.seek(header.pos, Seek::Beg);
-    out << header.size;
+    ofs.seekp(header.pos, std::ios_base::beg);
+    ofs.write(reinterpret_cast<const char*>(&header.size), sizeof(header.size));
 
     for (const BufferOffset& item : offset)
     {
-        out.seek(item.pos, Seek::Beg);
-        out << item.offset << item.size;
-    }
+        ofs.seekp(item.pos, std::ios_base::beg);
 
-    ofs.write(reinterpret_cast<const char*>(out.getDataConst().data()), out.getDataConst().size());
+        ofs.write(reinterpret_cast<const char*>(&item.offset), sizeof(item.offset));
+        ofs.write(reinterpret_cast<const char*>(&item.size), sizeof(item.size));
+    }
 
     return true;
 }
 
 bool BaseHeader::read(std::ifstream& ifs)
 {
-    char ch = -1;
-    m_type = "";
-    while (1)
+    std::string type = readCString(ifs);
+    if (m_type.size() && m_type != type)
     {
-        ifs.read(&ch, 1);
-        if (ch)
-        {
-            m_type += ch;
-        }
-        else
-        {
-            break;
-        }
+        return false;
     }
 
     uint8_t verMajor = 0;
     uint8_t verMinor = 0;
     ifs.read(reinterpret_cast<char*>(&verMajor), 1);
     ifs.read(reinterpret_cast<char*>(&verMinor), 1);
-
     if (verMajor != getVersionMajor() || verMinor != getVersionMinor())
     {
         return false;
@@ -227,35 +393,30 @@ bool BaseHeader::read(std::ifstream& ifs)
     BufferOffset header;
     ifs.read(reinterpret_cast<char*>(&header.size), sizeof(header.size));
 
-    DataBuffer buff("");
-    buff.read(ifs, header.size);
-
-    uint8_t count = 0;
-    buff >> count;
-
+    uint16_t count = 0;
+    ifs.read(reinterpret_cast<char*>(&count), sizeof(count));
     for (int ii = 0; ii < count; ++ii)
     {
-        std::string name;
-        std::string marker;
         BufferOffset offset;
+        std::string marker = readCString(ifs);
+        std::string name = readCString(ifs);
 
-        buff >> marker;
-        buff >> name;
-        buff >> offset.offset;
-        buff >> offset.size;
+        ifs.read(reinterpret_cast<char*>(&offset.offset), sizeof(offset.offset));
+        ifs.read(reinterpret_cast<char*>(&offset.size), sizeof(offset.size));
 
         BaseBuffer* bb = bufferFabric(name, marker);
-
         if (!bb)
         {
-            // TODO need to add an error
+            SU_ASSERT(true);
             continue;
         }
 
+        auto pos = ifs.tellg();
         ifs.seekg(offset.offset, std::ios_base::beg);
         bb->read(ifs, offset.size);
-
         m_buffer.push_back(bb);
+
+        ifs.seekg(pos, std::ios_base::beg);
     }
 
     return true;
@@ -265,14 +426,26 @@ BaseBuffer* BaseHeader::bufferFabric(const std::string& name, const std::string&
 {
     if (marker == StringBuffer::marker())
     {
-        return new StringBuffer(name);
+        return new StringBuffer(name, this);
     }
 
     if (marker == DataBuffer::marker())
     {
-        return new DataBuffer(name);
+        return new DataBuffer(name, this);
     }
 
+    return nullptr;
+}
+
+BaseBuffer* BaseHeader::getBuffer(const std::string& name)
+{
+    for (auto item : m_buffer)
+    {
+        if (item->getName() == name)
+        {
+            return item;
+        }
+    }
     return nullptr;
 }
 
--- filesystem.h
+++ filesystem.h
@@ -2,12 +2,27 @@
 #define _SMALLUTILL_FILESYSTEM_H_
 #pragma once
 
+//
+// Store and load unsigned integer values without packing
+//
+//#define SU_FILESYSTEM_NOT_USING_PACKING
+
 #include <string>
 #include <vector>
 #include <fstream>
 
 using StringArray = std::vector<std::string>;
 using ByteArray = std::vector<uint8_t>;
+
+#ifndef SU_ASSERT
+#ifdef _DEBUG
+#define SU_ASSERT(_Expression) (void)( (!!(_Expression)) || (__debugbreak(), 1) )
+#define SU_BREAKPOINT() (__debugbreak())
+#else
+#define SU_ASSERT(x) ((void)0)
+#define SU_BREAKPOINT() ((void)0)
+#endif
+#endif
 
 namespace su
 {
@@ -21,64 +36,98 @@ enum class Seek
     End,
 };
 
+class BaseHeader;
 
 class BaseBuffer
 {
 public:
-    BaseBuffer(const std::string& name) : m_name(name) {}
+    BaseBuffer(const std::string& name, const BaseHeader* parent = nullptr) : m_name(name), m_parent(parent) {}
     virtual ~BaseBuffer() = default;
 
     virtual const std::string& getName() const
         { return m_name; }
 
     virtual const std::string& getMarker() = 0;
-
-    virtual ByteArray getData() const = 0;
-
     virtual bool read(std::ifstream& ifs, uint64_t size) = 0;
+    virtual bool write(std::ofstream& ofs) const = 0;
+
+    virtual void setParent(BaseHeader* hdr)
+        { m_parent = hdr; }
+
+    virtual const BaseHeader* getParent() const
+        { return m_parent; }
 
 protected:
-    std::string m_name;
+    std::string m_name = "";
+    const BaseHeader* m_parent = nullptr;
 };
+
+//
 
 class DataBuffer : public BaseBuffer
 {
 public:
-    DataBuffer(const std::string& name) : BaseBuffer(name) {}
+    DataBuffer(const std::string& name, const BaseHeader* parent = nullptr) : BaseBuffer(name, parent) {}
     virtual ~DataBuffer() = default;
 
+    // BaseBuffer
+    virtual const std::string& getMarker() override { return marker(); }
+    virtual bool read(std::ifstream& ifs, uint64_t size) override;
+    virtual bool write(std::ofstream& ofs) const override;
+
+    // DataBuffer
     DataBuffer& operator << (bool val) { uint8_t v = !!val; return add(&v, sizeof(v)); }
+    DataBuffer& operator << (char val) { return add(&val, sizeof(val)); }
     DataBuffer& operator << (int8_t val) { return add(&val, sizeof(val)); }
-    DataBuffer& operator << (uint8_t val) { return add(&val, sizeof(val)); }
     DataBuffer& operator << (int16_t val) { return add(&val, sizeof(val)); }
-    DataBuffer& operator << (uint16_t val) { return add(&val, sizeof(val)); }
     DataBuffer& operator << (int32_t val) { return add(&val, sizeof(val)); }
-    DataBuffer& operator << (uint32_t val) { return add(&val, sizeof(val)); }
     DataBuffer& operator << (int64_t val) { return add(&val, sizeof(val)); }
+
+#ifndef SU_FILESYSTEM_NOT_USING_PACKING
+    DataBuffer& operator << (uint8_t val) { return addPackUnsigned(val); }
+    DataBuffer& operator << (uint16_t val) { return addPackUnsigned(val); }
+    DataBuffer& operator << (uint32_t val) { return addPackUnsigned(val); }
+    DataBuffer& operator << (uint64_t val) { return addPackUnsigned(val); }
+#else
+    DataBuffer& operator << (uint8_t val) { return add(&val, sizeof(val)); }
+    DataBuffer& operator << (uint16_t val) { return add(&val, sizeof(val)); }
+    DataBuffer& operator << (uint32_t val) { return add(&val, sizeof(val)); }
     DataBuffer& operator << (uint64_t val) { return add(&val, sizeof(val)); }
+#endif
+    
     DataBuffer& operator << (float val) { return add(&val, sizeof(val)); }
     DataBuffer& operator << (double val) { return add(&val, sizeof(val)); }
     DataBuffer& operator << (const std::string& val)
     {
-        *this << static_cast<uint32_t>(val.size());
+        *this << val.size();
         add(val.c_str(), val.size());
         return *this;
     }
 
     DataBuffer& operator >> (bool& val) { uint8_t v = 0; get(&v, sizeof(v)); val = v != 0; return *this; }
+    DataBuffer& operator >> (char& val) { return get(&val, sizeof(val)); }
     DataBuffer& operator >> (int8_t& val) { return get(&val, sizeof(val)); }
-    DataBuffer& operator >> (uint8_t& val) { return get(&val, sizeof(val)); }
     DataBuffer& operator >> (int16_t& val) { return get(&val, sizeof(val)); }
-    DataBuffer& operator >> (uint16_t& val) { return get(&val, sizeof(val)); }
     DataBuffer& operator >> (int32_t& val) { return get(&val, sizeof(val)); }
-    DataBuffer& operator >> (uint32_t& val) { return get(&val, sizeof(val)); }
     DataBuffer& operator >> (int64_t& val) { return get(&val, sizeof(val)); }
+
+#ifndef SU_FILESYSTEM_NOT_USING_PACKING
+    DataBuffer& operator >> (uint8_t& val) { uint64_t v; getPackUnsigned(v); val = static_cast<uint8_t>(v); return *this; }
+    DataBuffer& operator >> (uint16_t& val) { uint64_t v; getPackUnsigned(v); val = static_cast<uint16_t>(v); return *this; }
+    DataBuffer& operator >> (uint32_t& val) { uint64_t v; getPackUnsigned(v); val = static_cast<uint32_t>(v); return *this; }
+    DataBuffer& operator >> (uint64_t& val) { return getPackUnsigned(val); }
+#else
+    DataBuffer& operator >> (uint8_t& val) { return get(&val, sizeof(val)); }
+    DataBuffer& operator >> (uint16_t& val) { return get(&val, sizeof(val)); }
+    DataBuffer& operator >> (uint32_t& val) { return get(&val, sizeof(val)); }
     DataBuffer& operator >> (uint64_t& val) { return get(&val, sizeof(val)); }
+#endif
+
     DataBuffer& operator >> (float& val) { return get(&val, sizeof(val)); }
     DataBuffer& operator >> (double& val) { return get(&val, sizeof(val)); }
     DataBuffer& operator >> (std::string& val)
     {
-        uint32_t size = 0;
+        size_t size = 0;
         *this >> size;
         val.resize(size);
         get(val.data(), val.size());
@@ -86,11 +135,13 @@ public:
     }
 
     DataBuffer& add(const void* buf, size_t size);
+    DataBuffer& addCString(const char* c_str);
+    DataBuffer& addPackUnsigned(uint64_t v);
     DataBuffer& get(void* buf, size_t size);
+    DataBuffer& getCString(std::string& str);
+    DataBuffer& getPackUnsigned(uint64_t& v);
 
-    virtual ByteArray getData() const override
-        { return m_data; }
-    virtual const ByteArray& getDataConst() const
+    virtual const ByteArray& array() const
         { return m_data; }
 
     virtual bool isEof() const
@@ -100,10 +151,6 @@ public:
         { return m_pos; }
 
     virtual void seek(int64_t pos, Seek way);
-    virtual bool read(std::ifstream& ifs, uint64_t size) override;
-
-    virtual const std::string& getMarker() override
-        { return marker(); }
 
     static const std::string& marker()
         { return m_marker; }
@@ -118,17 +165,17 @@ protected:
 class StringBuffer : public BaseBuffer
 {
 public:
-    StringBuffer(const std::string& name) : BaseBuffer(name) {}
+    StringBuffer(const std::string& name, const BaseHeader* parent = nullptr) : BaseBuffer(name, parent) {}
     virtual ~StringBuffer() = default;
 
+    // BaseBuffer
+    virtual const std::string& getMarker() override { return marker(); }
+    virtual bool read(std::ifstream& ifs, uint64_t size) override;
+    virtual bool write(std::ofstream& ofs) const override;
+
+    //
     uint32_t add(const std::string& str);
     std::vector<uint32_t> add(const StringArray& v);
-
-    virtual ByteArray getData() const override;
-    virtual bool read(std::ifstream& ifs, uint64_t size) override;
-
-    virtual const std::string& getMarker() override
-        { return marker(); }
 
     static const std::string& marker()
         { return m_marker; }
@@ -143,7 +190,7 @@ class BaseHeader
 {
 public:
     BaseHeader(const std::string& type) : m_type(type) {}
-    virtual ~BaseHeader() = default;
+    virtual ~BaseHeader();
 
     void add(BaseBuffer* buf)
         { m_buffer.push_back(buf); }
@@ -156,10 +203,12 @@ public:
     bool write(std::ofstream& ofs) const;
     bool read(std::ifstream& ifs);
 
+    BaseBuffer* getBuffer(const std::string& name);
+
 protected:
     struct BufferOffset
     {
-        uint64_t pos;
+        std::streampos pos;
         uint64_t offset;
         uint32_t size;
     };

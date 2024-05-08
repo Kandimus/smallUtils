7b96a2358b77c737a5b7719b1068f7585e58e2b0
Vitalii Litvinov <vitalii.litvinov@thetoadman.com>
2024-05-08 18:17:10 +0200

    added ScopeExit class

---
+++ scopeexit.h
@@ -0,0 +1,54 @@
+// Copyright (C) Nikita Shipovalov
+
+#pragma once
+
+namespace su
+{
+
+/*
+    The purpose of this thing is to ensure code execution on scope exit
+    The preferred usage is with macro below (less code needed):
+
+    SU_ON_SCOPE_EXIT( do_Some_Stuff )
+*/
+
+template <typename F>
+class ScopeExit
+{
+public:
+    ScopeExit( ScopeExit&& ) = default;
+
+    ScopeExit( F&& f ) : m_f( std::move(f) ) {}
+    ~ScopeExit() { m_f(); }
+ 
+private:
+    ScopeExit(const ScopeExit&) = delete;
+    ScopeExit& operator=(const ScopeExit&) = delete;
+    ScopeExit& operator=(ScopeExit&&) = delete;
+
+    F m_f;
+};
+
+template <typename F>
+ScopeExit<F> makeScopeExit( F&& f )
+{
+    return ScopeExit<F>( std::move(f) );
+};
+
+
+} //namespace su
+
+/*
+    Utility macro to help adding several scope exiters in the same scope:
+
+    {
+        Class* var = new Class();
+        SU_ON_SCOPE_EXIT( delete var; )
+        stuff();
+        SU_ON_SCOPE_EXIT( anotherStuffOnScopeExit(); )
+    } // <- execution of scope exiters is here in the same order as they were declared
+*/
+#define SU_STRING_JOIN2(arg1, arg2)    SU_DO_STRING_JOIN2(arg1, arg2)
+#define SU_DO_STRING_JOIN2(arg1, arg2) arg1 ## arg2
+#define SU_ON_SCOPE_EXIT(code)         const auto SU_STRING_JOIN2(scope_exit_, __LINE__) = su::makeScopeExit( [&](){ code; } )
+

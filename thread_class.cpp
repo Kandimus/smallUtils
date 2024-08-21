723e68ebbb75184b45d98e5f6f95ab7b52f58da0
Vitalii Litvinov <vitalii.litvinov@thetoadman.com>
2024-08-21 17:58:14 +0200

    ThreadClass class

---
+++ thread_class.cpp
@@ -0,0 +1,101 @@
+
+#include "thread_class.h"
+
+namespace su
+{
+
+ThreadClass::ThreadClass()
+    : m_delay(100), m_status(Status::Undef), m_command(Command::None)
+{
+}
+
+ThreadClass::~ThreadClass()
+{
+    close();
+}
+
+std::thread* ThreadClass::run(size_t delay)
+{
+    close();
+
+    m_delay.store(delay);
+
+    m_thread = new std::thread(ThreadClass::ThreadFunc, this); //TODO
+    m_status.store(Status::Running);
+    return m_thread;
+}
+
+void ThreadClass::close()
+{
+    if (!m_thread)
+    {
+        m_status.store(Status::Closed);
+        return;
+    }
+
+    Status status = m_status.load();
+    if (status == Status::Paused || status == Status::Running)
+    {
+        m_command.store(Command::Finish);
+        m_thread->join();
+
+        delete m_thread;
+        m_thread = nullptr;
+    }
+
+    m_status.store(Status::Closed);
+}
+
+void ThreadClass::pause()
+{
+    if (m_status.load() == Status::Running)
+    {
+        m_command.store(Command::Pause);
+    }
+}
+
+void ThreadClass::proccesing()
+{
+    while (1)
+    {
+        Command command = m_command.load();
+        size_t delay = m_delay.load();
+        bool isPaused = false;
+
+        m_command.store(Command::None);
+
+        switch (command)
+        {
+            case Command::Finish: m_status.store(Status::Finished); doFinished(); return;
+            case Command::Pause:  m_status.store(Status::Paused); isPaused = true; break;
+            default: break;
+        }
+
+        if (isPaused)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(delay < 16 ? 16 : delay));
+            continue;
+        }
+
+        m_status.store(Status::Running);
+
+        if (delay)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+        }
+
+        doWork();
+    }
+}
+
+void ThreadClass::ThreadFunc(ThreadClass* tc)
+{
+    if (!tc)
+    {
+        return;
+    }
+
+    tc->proccesing();
+}
+
+} // namespace su
---
+++ thread_class.h
@@ -0,0 +1,59 @@
+
+#pragma once
+
+#include <thread>
+#include <vector>
+#include <atomic>
+
+namespace su
+{
+
+class ThreadClass
+{
+public:
+    enum class Status
+    {
+        Undef = 0,
+        Finished,
+        Running,
+        Paused,
+        Closed,
+    };
+
+    enum class Command
+    {
+        None = 0,
+        Pause,
+        Finish,
+        Restore,
+    };
+
+    ThreadClass();
+    virtual ~ThreadClass();
+
+    Status status() const { return m_status.load(); };
+    std::thread* thread() const { return m_thread; }
+    bool isPaused() const { return m_status.load() == Status::Paused; }
+    bool isWork() const { auto status = m_status.load(); return status == Status::Running || status == Status::Paused; }
+    void finish() { m_command.store(Command::Finish); }
+    void restore() { m_command.store(Command::Restore); }
+    std::thread* run(size_t delay);
+    void close();
+    void pause();
+    
+    virtual void doWork() = 0;
+    virtual void doFinished() = 0;
+
+protected:
+    void proccesing();
+
+private:
+    std::thread* m_thread = nullptr;
+    std::atomic<size_t> m_delay;
+    std::atomic<Status>  m_status;
+    std::atomic<Command> m_command;
+
+    static void ThreadFunc(ThreadClass*);
+};
+
+} // namespace su
